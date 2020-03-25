47c5029b6bad15bc9abb55e83a83091d49a7979f
pythonpete32 <pythonpete32@gmail.com>
2020-03-25 00:00:51 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  sum: (value1, value2) => {
+    return value1 + value2;
+  }
+}
---
+++ sum.spec.js
@@ -0,0 +1,9 @@
+const  Index  = require('./index');
+
+
+describe('sum', function() {
+    it('adds 1 + 2 to equal 3', function() {
+      const sum = Index.sum
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
