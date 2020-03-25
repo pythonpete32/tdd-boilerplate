c180683adc1fd3d2bfb150fca0272edd0ad1d6a6
pythonpete32 <pythonpete32@gmail.com>
2020-03-25 21:13:54 +0000

    update config

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-import { sayHello } from "../src";
+import { sayHello } from "../src/index";
 console.log(sayHello)
 test("sayHello", () => {
   expect(sayHello("foo")).toBe("Hello, foo!");
---
+++ wallaby.js
@@ -0,0 +1,16 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.js', instrument: false},
+      'src/**/*.js'
+    ],
+
+    tests: [
+      'test/**/*.spec.js'
+    ],
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel()
+    }
+  };
+};
